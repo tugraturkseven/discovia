d81069fb14f8cb8c4d928c7ffd99c5c1d0e2ff31
Tugra <tugraturkseven@hotmail.com>
2023-10-17 20:29:14 +0300

    PostCard category icon added. User section design updated.

--- components/PostCard.js
+++ components/PostCard.js
@@ -5,20 +5,83 @@ import { useTheme } from 'react-native-paper';
 const PostCard = (props) => {
 
     const { colors } = useTheme();
-
-
-
-
+    const emojiDict = {
+        'Route': {
+            icon: '🗺️',
+        },
+        'Restaurant': {
+            icon: '🥗',
+        },
+        'Events': {
+            icon: '🎉',
+        },
+        'Nightlife': {
+            icon: '🍺',
+        },
+        'Museum': {
+            icon: '🏛️',
+        },
+        'Parks': {
+            icon: '🏞️',
+        },
+        'Camping': {
+            icon: '🏕️',
+        },
+        'Temples': {
+            icon: '🕯️',
+        },
+        'Shopping': {
+            icon: '🛍️',
+        },
+        'Beach': {
+            icon: '🏖️',
+        }
+    }
 
     return (
         <View style={styles.container}>
             <ImageBackground source={props.img} resizeMode='cover' style={styles.card}>
                 <View style={{ flex: 1, justifyContent: 'flex-end', padding: 16 }}>
-                    <Text style={{ color: colors.onBackground, fontSize: 24, fontWeight: 'bold' }}>{props.title}</Text>
-                    <Text style={{ color: colors.onBackground, fontSize: 16 }}>{props.city}</Text>
+                    <Text
+                        style={{
+                            color: colors.onBackground,
+                            fontSize: 24,
+                            fontWeight: 'bold',
+                            textShadowColor: 'rgba(0, 0, 0, 0.85)', // Shadow color
+                            textShadowOffset: { width: 2, height: 2 }, // Shadow offset
+                            textShadowRadius: 2, // Shadow radius
+                        }}
+                    >
+                        {props.title}
+                    </Text>
+                    <Text
+                        style={{
+                            color: colors.onBackground,
+                            fontSize: 16,
+                            textShadowColor: 'rgba(0, 0, 0, 0.85)', // Shadow color
+                            textShadowOffset: { width: 2, height: 2 }, // Shadow offset
+                            textShadowRadius: 2, // Shadow radius
+                        }}
+                    >
+                        {props.city}
+                    </Text>
+                </View>
+                <View style={{ flex: 1, justifyContent: 'flex-end', padding: 16, }}>
+                    <Text
+                        style={{
+                            fontSize: 36,
+                            textAlign: 'right',
+                            textShadowColor: 'rgba(0, 0, 0, 0.5)', // Shadow color
+                            textShadowOffset: { width: 2, height: 2 }, // Shadow offset
+                            textShadowRadius: 2, // Shadow radius
+                        }}
+                    >
+                        {emojiDict[props.category].icon}
+                    </Text>
                 </View>
             </ImageBackground>
         </View>
+
     )
 }
 
@@ -30,6 +93,7 @@ const styles = StyleSheet.create({
     },
     card: {
         flex: 1,
+        flexDirection: 'row',
         borderRadius: 8,
         overflow: 'hidden',
         elevation: 4,
--- screens/Profile.js
+++ screens/Profile.js
@@ -10,9 +10,9 @@ const Profile = () => {
     const giresun = { uri: 'https://cdn.goturkiye.com/giresun/see-desktop-kapak.jpg' };
     const restaurant = { uri: 'https://algedra.com.tr/assets/imgs/upload2/Sophisticated%20Restaurant%20Design/imagesforwebsite92_62df98aa3887a.jpg' };
     const concert = { uri: 'https://images.themagger.net/wp-content/uploads/2019/01/Istanbul-Konserleri-633x433.jpeg' };
-
-
-
+    const pub = { uri: 'https://www.foodandwine.com/thmb/8rtGtUmtC0KiJCDxAUXP_cfwgM8=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/GTM-Best-US-Bars-Katana-Kitten-FT-BLOG0423-fa9f2ba9925c47abb4afb0abd25d915a.jpg' };
+    const yerebatanSarnici = { uri: 'https://yerebatan.com/wp-content/uploads/2022/12/yerebatan-sergi-ogu6525-min-JlPk-scaled-1.jpg' };
+    const boluAbant = { uri: 'https://cdn.seturday.com/content/uploads/makale_resimleri/kapak/sonbahar-deyince-akla-gelen-abant-golu-tabiat-parki-bolu-290d6747-4dce-49da-bf53-4d9d7c7243a5banner.jpg' };
 
     return (
         <View style={[styles.container, { backgroundColor: colors.background }]}>
@@ -20,19 +20,38 @@ const Profile = () => {
             <View style={[styles.userSection, { borderBottomWidth: 1, borderBottomColor: colors.backdrop }]}>
 
                 <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
-                    <UserAvatar size={96} />
-                    <View style={{ flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
-                        <Text variant='titleMedium'>Tugra Turkseven</Text>
-                        <Text variant='bodyMedium' style={{ color: colors.onBackground, opacity: 0.5 }}>@tugraturkseven</Text>
-                        <Text variant='bodyMedium' style={{ color: colors.onBackground, opacity: 0.5 }}>Istanbul, Turkey</Text>
+                    <View style={{ flex: 1 }}>
+                        <UserAvatar size={96} />
                     </View>
-                    <View style={{ flexDirection: 'column', alignItems: 'center', justifyContent: 'space-evenly' }}>
-                        <Text variant='bodyLarge' style={{ color: colors.onBackground }}>10</Text>
-                        <Text variant='bodyLarge' style={{ color: colors.onBackground, opacity: 0.5 }}>Post</Text>
-                        <Text variant='bodyLarge' style={{ color: colors.onBackground }}>100</Text>
-                        <Text variant='bodyLarge' style={{ color: colors.onBackground, opacity: 0.5 }}>Followers</Text>
+
+                    <View style={{ flex: 2, flexDirection: 'column', }}>
+                        <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignContent: 'center' }}>
+
+                            <View>
+                                <Text variant='titleMedium' style={[styles.textStyle, { color: colors.onBackground }]}>6</Text>
+                                <Text variant='bodyMedium' style={[styles.textStyle, { color: colors.onBackground }]}>Posts</Text>
+                            </View>
+                            <View>
+                                <Text variant='titleMedium' style={[styles.textStyle, { color: colors.onBackground }]}>3</Text>
+                                <Text variant='bodyMedium' style={[styles.textStyle, { color: colors.onBackground }]}>Followers</Text>
+                            </View>
+                            <View>
+                                <Text variant='titleMedium' style={[styles.textStyle, { color: colors.onBackground }]}>10</Text>
+                                <Text variant='bodyMedium' style={[styles.textStyle, { color: colors.onBackground }]}>Following</Text>
+                            </View>
+                        </View>
+
+                        <View style={{ flexDirection: 'column', paddingVertical: 8 }}>
+                            <Text variant='titleLarge' style={{ color: colors.onBackground }}>Tugra Turkseven</Text>
+                            <Text variant='titleSmall' style={{ color: colors.outline }}>@tugraturkseven</Text>
+                        </View>
+
                     </View>
+
+
                 </View>
+
+
                 <View style={{ flexDirection: 'column', justifyContent: 'center', alignItems: 'center', paddingVertical: 16 }}>
                     <Text variant='bodyLarge' style={{ color: colors.onBackground }}>A software engineer who likes to travel around. 🛫</Text>
                 </View>
@@ -44,9 +63,12 @@ const Profile = () => {
             </View>
 
             <ScrollView showsVerticalScrollIndicator={false}>
-                <PostCard img={giresun} title='Karadeniz Turu' city='Giresun, Turkey.' />
-                <PostCard img={restaurant} title='Boğazda Yemek' city='Istanbul, Turkey.' />
-                <PostCard img={concert} title='Duman' city='Marmaris, Turkey.' />
+                <PostCard img={giresun} title='Karadeniz Turu' city='Giresun, Turkey.' category='Route' />
+                <PostCard img={restaurant} title='Boğazda Yemek' city='Istanbul, Turkey.' category='Restaurant' />
+                <PostCard img={concert} title='Duman' city='Marmaris, Turkey.' category='Events' />
+                <PostCard img={pub} title='Beerhall' city='Eskişehir, Turkey.' category='Nightlife' />
+                <PostCard img={yerebatanSarnici} title='Yerebatan Sarnıcı' city='Istanbul, Turkey.' category='Museum' />
+                <PostCard img={boluAbant} title='Abant Gölü' city='Bolu, Turkey.' category='Parks' />
             </ScrollView>
 
 
@@ -65,6 +87,10 @@ const styles = StyleSheet.create({
     profileButtons: {
         flex: 1,
         marginHorizontal: 8,
+    },
+    textStyle: {
+        textAlign: 'center',
+        fontWeight: '500'
     }
 
 })
